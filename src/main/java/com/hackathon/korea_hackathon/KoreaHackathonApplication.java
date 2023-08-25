7e52d8d26eeeec31170cc1a07ff4cbad81031e5c
HyunSu1768 <azxcv1768@gmail.com>
2023-08-25 12:57:04 +0900

    first commit

---
+++ src/main/java/com/hackathon/korea_hackathon/KoreaHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.korea_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreaHackathonApplication.class, args);
+    }
+
+}
