8de81c87db49ba36ebd82ee41c9add0b3c7f1ae4
HyunSu1768 <azxcv1768@gmail.com>
2023-08-27 04:18:50 +0900

    cd

--- src/main/java/com/hackathon/korea_hackathon/domain/ship/service/ShipLogService.java
+++ src/main/java/com/hackathon/korea_hackathon/domain/ship/service/ShipLogService.java
@@ -206,13 +206,13 @@ public class ShipLogService {
         ));
 
         locationResponses.add(new LocationResponse(
-                Double.toString(Double.parseDouble(curLocation.getLatitude()) + 0.2),
-                Double.toString(Double.parseDouble(curLocation.getLongitude()) - 0.2)
+                Double.toString(Double.parseDouble(curLocation.getLatitude()) + 0.05),
+                Double.toString(Double.parseDouble(curLocation.getLongitude()) - 0.05)
         ));
 
         locationResponses.add(new LocationResponse(
-                Double.toString(Double.parseDouble(curLocation.getLatitude()) + 0.5),
-                Double.toString(Double.parseDouble(curLocation.getLongitude()) + 0.4)
+                Double.toString(Double.parseDouble(curLocation.getLatitude()) + 0.02),
+                Double.toString(Double.parseDouble(curLocation.getLongitude()) + 0.02)
         ));
         return locationResponses;
     }
